73ce9c92be0e771acca574640d5f1fceadadfd22
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-21 14:39:55 -0400

    printed numbers

--- Topic 8 assignment/Program.cs
+++ Topic 8 assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Topic_8_assignment
+﻿using System.Runtime.CompilerServices;
+
+namespace Topic_8_assignment
 {
     internal class Program
     {
@@ -6,19 +8,43 @@
         {
             Random generator = new Random();
 
+            int max, min, numbersNeeded;
+
             //Assignment one
-            Console.WriteLine("Assignment one - Reading in Colours");
+            //Console.WriteLine("Assignment one - Reading in Colours");
+            //Console.WriteLine();
+            //List<string> colours = new List<string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Please enter a colour: ");
+            //    colours.Add(Console.ReadLine());
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(string.Join(", ", colours));
+            //Console.WriteLine();
+            //Console.WriteLine(colours[generator.Next(0, 6)]);
+            //Console.ReadLine();
+            //Console.Clear();
+
+            //Assignment two
+            Console.WriteLine("Assignment two - Random Numbers");
             Console.WriteLine();
-            List<string> colours = new List<string>();
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("How many numbers do you need?");
+            int.TryParse(Console.ReadLine(), out numbersNeeded);
+            Console.WriteLine();
+            Console.WriteLine("What is the maximum value?");
+            int.TryParse(Console.ReadLine(), out max);
+            Console.WriteLine();
+            Console.WriteLine("What is the minimum value?");
+            int.TryParse(Console.ReadLine(), out min);
+            Console.WriteLine();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < numbersNeeded; i++)
             {
-                Console.WriteLine("Please enter a colour: ");
-                colours.Add(Console.ReadLine());
+                numbers.Add(generator.Next(min, (max + 1)));
             }
-            Console.WriteLine();
-            Console.WriteLine(string.Join(", ", colours));
-            Console.WriteLine();
-            Console.WriteLine(colours[generator.Next(0, 6)]);
+
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }
