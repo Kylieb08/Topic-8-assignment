6689d64c14ef74b3f1cc04f6ecc60bc005f25b81
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-24 14:08:36 -0400

    finished assignment two

--- Topic 8 assignment/Program.cs
+++ Topic 8 assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace Topic_8_assignment
 {
@@ -8,26 +9,30 @@ namespace Topic_8_assignment
         {
             Random generator = new Random();
 
-            int max, min, numbersNeeded;
+            int max, min, numbersNeeded, userNumber, userNumberCount = 0, numberEqualZero;
 
             //Assignment one
-            //Console.WriteLine("Assignment one - Reading in Colours");
-            //Console.WriteLine();
-            //List<string> colours = new List<string>();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine("Please enter a colour: ");
-            //    colours.Add(Console.ReadLine());
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine(string.Join(", ", colours));
-            //Console.WriteLine();
-            //Console.WriteLine(colours[generator.Next(0, 6)]);
-            //Console.ReadLine();
-            //Console.Clear();
+            Console.WriteLine("Assignment one - Reading in Colours");
+            Console.WriteLine();
+            List<string> colours = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please enter a colour: ");
+                colours.Add(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", colours));
+            Console.WriteLine();
+            Console.WriteLine(colours[generator.Next(0, 6)]);
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
 
             //Assignment two
             Console.WriteLine("Assignment two - Random Numbers");
+            Console.WriteLine();
+            Console.WriteLine("Creating a list");
             Console.WriteLine();
             Console.WriteLine("How many numbers do you need?");
             int.TryParse(Console.ReadLine(), out numbersNeeded);
@@ -45,6 +50,68 @@ namespace Topic_8_assignment
             }
 
             Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Counting occurrences of a number chosen by the user
+            Console.WriteLine("Counting occurrences of a number chosen by the user");
+            Console.WriteLine();
+            Console.WriteLine("Please enter an integer:");
+            int.TryParse(Console.ReadLine(), out userNumber);
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == userNumber)
+                {
+                    userNumberCount += 1;
+                }
+            }
+            Console.WriteLine(userNumber + " appears " + userNumberCount + " times");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Resetting a number chosen by the user to zero
+            Console.WriteLine();
+            Console.WriteLine("Resetting a number chosen by the user to zero");
+            Console.WriteLine();
+            Console.WriteLine("Please enter an integer:");
+            int.TryParse(Console.ReadLine(), out numberEqualZero);
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numberEqualZero)
+                {
+                    numbers[i] = 0;
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Resetting everything to zero
+            Console.WriteLine();
+            Console.WriteLine("Resetting everything to zero");
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] = 0;
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Refilling the list with random numbers
+            Console.WriteLine();
+            Console.WriteLine("Refilling the list with random numbers");
+            Console.WriteLine();
+            for (int i = 0; i < numbersNeeded; i++)
+            {
+                numbers[i] = (generator.Next(min, (max + 1)));
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
